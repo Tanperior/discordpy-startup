93ce817fefe1437eb2e1c4c70248d57f04d88357
TAN90 <65294372+Tanperior@users.noreply.github.com>
2020-05-14 02:08:06 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -33,5 +33,32 @@ async def pop(ctx):
 async def 草蛇(ctx):
     await ctx.send('ジャローダ')
 
+@client.event
+async def on_ready():
+    print('ready')
+
+# 指定時間に走る処理
+async def SendMessage():
+    channel = client.get_channel(CHANNEL_ID)
+    await channel.send('時間だよ')
+
+# 30秒に一回ループ
+@tasks.loop(seconds=30)
+async def time_check():
+    sleepTime = 0
+    # 現在の時刻
+    now = datetime.now().strftime('%H:%M')
+    if now in dateTimeList :
+        print(now)
+        await SendMessage()
+        #該当時間だった場合は２重に投稿しないよう３０秒余計に待機
+        await asyncio.sleep(30)
+
+
+
+#ループ処理
+time_check.start()
+
+
 bot.run(token)
 
