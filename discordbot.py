e33570f8049095e08910d3259b6e77c869a818d9
TAN90 <65294372+Tanperior@users.noreply.github.com>
2020-05-14 00:52:46 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -12,37 +12,10 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-    #投稿する日時
-dateTimeList = [
-'01:00',
-'05:00',
-'09:00',
-'13:00',
-'17:00',
-'21:00',
-]
-CHANNEL_ID = 710111629806927943
-@bot.command()
-async def ping(ctx):    
-    await ctx.send('pong')
-    
-# 指定時間に走る処理
-async def SendMessage():
-    channel = client.get_channel(CHANNEL_ID)
-    await channel.send('収入が利用可能です！　@メンバー')
 
-# 30秒に一回ループ
-@tasks.loop(seconds=30)
-async def time_check():
-    sleepTime = 0
-    # 現在の時刻
-    now = datetime.now().strftime('%H:%M')
-    if now in dateTimeList :
-        print(now)
-        await SendMessage()
-        #該当時間だった場合は２重に投稿しないよう３０秒余計に待機
-        await asyncio.sleep(30)
-#ループ処理
-time_check.start()
+@bot.command()
+async def pop(ctx):
+    await ctx.send('pong')
+
 
 bot.run(token)
