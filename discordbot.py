e524bb7ca0040b7143323eaac15407cce062c480
TAN90 <65294372+Tanperior@users.noreply.github.com>
2020-05-14 01:45:43 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,23 +16,6 @@ dateTimeList = [
 '21:00',
 ]
 
-# 役職を付与する非同期関数を定義
-async def grant_role(payload):
-    # 絵文字が異なる場合は処理を打ち切る
-    if payload.emoji.name != EMOJI_ACTIVE: 
-        return
-
-    # チャンネルが異なる場合は処理を打ち切る
-    channel = client.get_channel(payload.channel_id)
-    if channel.id != ID_CHANNEL_WELCOME:
-        return
-
-    # Member オブジェクトと Role オブジェクトを取得して役職を付与
-    member = channel.guild.get_member(payload.user_id)
-    role = guild.get_role(ID_ROLE_WELCOME)
-    await member.add_roles(role)
-    return member
-
 
 
 @bot.event
@@ -41,17 +24,11 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
+
 @bot.command()
 async def pop(ctx):
     await ctx.send('pong')
 
-@client.event
-async def on_raw_reaction_add(payload):
-    # 役職を付与する非同期関数を実行して Optional[Member] オブジェクトを取得
-    member = await grant_role(payload)
-    if member is not None: # 役職を付与したメンバーがいる時
-        text = f'{member.mention} ようこそ！'
-        await message.channel.send(text)
-
 
 bot.run(token)
+
