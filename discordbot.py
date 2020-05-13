9c9816efa5431c00da272ecc1a6a3e3c878cd856
TAN90 <65294372+Tanperior@users.noreply.github.com>
2020-05-14 01:12:52 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,7 +5,8 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 CHANNEL_ID = 710111629806927943
-
+ID_ROLE_ACTIVE =710161784190795808
+EMOJI_ACTIVE = '✅'
 dateTimeList = [
 '01:00',
 '05:00',
@@ -25,6 +26,32 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def pop(ctx):
     await ctx.send('pong')
+    
+# 役職を付与する非同期関数を定義
+async def grant_role(payload):
+    # 絵文字が異なる場合は処理を打ち切る
+    if payload.emoji.name != EMOJI_ACTIVE: 
+        return
+
+    # チャンネルが異なる場合は処理を打ち切る
+    channel = client.get_channel(payload.channel_id)
+    if channel.id != ID_CHANNEL_README:
+        return
+
+    # Member オブジェクトと Role オブジェクトを取得して役職を付与
+    member = channel.guild.get_member(payload.user_id)
+    role = guild.get_role(ID_ROLE_ACTIVE)
+    await member.add_roles(role)
+    return member
+
+# リアクション追加時に実行されるイベントハンドラを定義
+@client.event
+async def on_raw_reaction_add(payload):
+    # 役職を付与する非同期関数を実行して Optional[Member] オブジェクトを取得
+    member = await grant_role(payload)
+    if member is not None: # 役職を付与したメンバーがいる時
+        text = f'{member.mention} ようこそ！'
+        await message.channel.send(text)
 
 
 bot.run(token)
