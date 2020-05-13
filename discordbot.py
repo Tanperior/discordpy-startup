2ac0d148511147985c4f709f01568344340eb9b4
TAN90 <65294372+Tanperior@users.noreply.github.com>
2020-05-14 03:04:36 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,9 +4,7 @@ import traceback
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
-CHANNEL_ID_WELCOME = 710111629806927943
-ID_ROLE_WELCOME =710161784190795808
-EMOJI_WELCOME = '✅'
+CHANNEL_ID = 710111629806927943
 dateTimeList = [
 '01:00',
 '05:00',
